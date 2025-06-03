732ade74f2edb1925ab1758337c29bba3c682ad6
faii <nurrahmanrifai3@gmail.com>
2025-06-03 09:18:20 +0700

    create database and table

---
+++ app/Filament/Resources/PklResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PklResource\Pages;
+use App\Filament\Resources\PklResource\RelationManagers;
+use App\Models\Pkl;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PklResource extends Resource
+{
+    protected static ?string $model = Pkl::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPkls::route('/'),
+            'create' => Pages\CreatePkl::route('/create'),
+            'edit' => Pages\EditPkl::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2025_06_03_020921_create_pkls_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pkls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('siswa_id')->constrained('siswas')->onDelete('restrict');
+            $table->foreignId('guru_id')->constrained('gurus');
+            $table->foreignId('industri_id')->constrained('industris')->onDelete('restrict');
+            $table->datetime('tanggal_mulai');
+            $table->datetime('tanggal_selesai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pkls');
+    }
+};
